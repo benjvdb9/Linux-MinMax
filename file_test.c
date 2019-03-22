310b1f898d2a4e4e024a7281ccf3f55e7e542e52
root <root@localhost.localdomain>
2019-03-22 22:00:57 +0100

    first

--- file_test.c
+++ file_test.c
@@ -1,5 +1,5 @@
 
-  #include <stdio.h> 
+  #include <stdio.h>
   #include<stdlib.h>
   #include<math.h>
 
@@ -14,29 +14,29 @@
         int *matrix;
         int ligne, col;
     };
-    
-    
-    struct MatrixLigneCol getDigits(FILE *file){
-        int buff[255] ; 
-        int i=0;
-    
-        printf("choisez le nombre de processus ");
-        scanf("%d",&n); 
 
-        if (file == NULL) 
-        {   
-        printf("Error! Could not open file\n"); 
+
+    struct MatrixLigneCol getDigits(FILE *file){
+        int buff[255] ;
+        int i=0;
+
+        printf("choisez le nombre de processus ");
+        scanf("%d",&n);
+
+        if (file == NULL)
+        {
+        printf("Error! Could not open file\n");
         exit(-1);
         }else{
         while (fscanf(file, "%d", buff+i) != EOF){
             size++;
             i++;
-        } 
+        }
         sizeArray=size/n;
-            int *intMatrix =(int *) malloc(n * sizeArray * sizeof(int)); 
+            int *intMatrix =(int *) malloc(n * sizeArray * sizeof(int));
             for(i=0;i<n;i++){
                 for(j=0;j<sizeArray;j++){
-                    *(intMatrix + i*sizeArray + j)=*(buff+k+j);    
+                    *(intMatrix + i*sizeArray + j)=*(buff+k+j);
                     }
                     k=k+sizeArray;
             }
@@ -46,17 +46,17 @@
             fileValueMa.ligne=n;
             fileValueMa.matrix=intMatrix;
 
-            return fileValueMa;   
+            return fileValueMa;
         }
 
     }
 
-    int main() 
-    { 
+    int main()
+    {
         FILE  * file = fopen("le_fichier.txt", "r");
 
-        
-     
+
+
 
         struct MatrixLigneCol filevalue;
         filevalue = getDigits(file);
@@ -64,7 +64,7 @@
         int sizeArray=filevalue.col;
         int n= filevalue.ligne;
         int *intMatrix = filevalue.matrix;
-        
+
      // int i;
         for(i=0;i<n;i++){
           for(j=0;j<sizeArray;j++)
@@ -74,6 +74,6 @@
 
         printf("% d  %d ",n  ,sizeArray);
         fclose(file);
-        return 0; 
-   
+        return 0;
+
       }
